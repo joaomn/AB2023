8eec0a7fb037309b7ab50f2ddd83d40bce3eb175
joaomn <joaoaraujomn@gmail.com>
2023-01-18 11:39:09 -0300

    versao1.0.2

--- agenda/src/main/java/br/com/blueapp/agenda/controllers/AgendaController.java
+++ agenda/src/main/java/br/com/blueapp/agenda/controllers/AgendaController.java
@@ -7,11 +7,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.blueapp.agenda.entitys.AgendaEntity;
@@ -28,7 +24,6 @@ import br.com.blueapp.agenda.services.AgendaService;
 import br.com.blueapp.agenda.services.excepcion.EmailCadastradoExcepcion;
 import br.com.blueapp.agenda.services.excepcion.PessoaNotExistsExcepcion;
 import io.swagger.annotations.ApiOperation;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/agenda")
--- agenda/src/main/java/br/com/blueapp/agenda/entitys/AgendaEntity.java
+++ agenda/src/main/java/br/com/blueapp/agenda/entitys/AgendaEntity.java
@@ -2,14 +2,14 @@ package br.com.blueapp.agenda.entitys;
 
 import java.io.Serializable;
 
-import br.com.blueapp.agenda.entitys.dtos.AgendaEntityDTO;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import br.com.blueapp.agenda.entitys.dtos.AgendaEntityDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
