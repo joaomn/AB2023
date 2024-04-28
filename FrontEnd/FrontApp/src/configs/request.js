6568d8aaf5b626553c49e7293c85953a6c53b7a4
joaomn <97440058+joaomn@users.noreply.github.com>
2024-04-28 07:28:17 -0300

    ajustes no compose

--- FrontEnd/FrontApp/src/configs/request.js
+++ FrontEnd/FrontApp/src/configs/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.API_URL || 'http://localhost:8080';
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 const axiosInstance = axios.create({
   baseURL,
